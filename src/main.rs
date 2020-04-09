dd7b580dbc9213d03744fb5569e9e462dd5b5147
tsudijon <timothysudijono@gmail.com>
2020-04-08 23:58:43 -0400

    Added deck class

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use deckofcards::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to HiLo!");
+
+    //prep the game
+    let mut deck = Deck::new();
+    deck.shuffle();
+
+
+    //first round
+
+    //second round
+    let mut group_score = 0;
+
+    //output score
 }
