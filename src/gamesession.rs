914a36f97056096f6283020a00627da6ef8c448a
tsudijon <timothysudijono@gmail.com>
2020-04-21 00:33:22 -0400

    Added Connect, Disconnect handlers, heartbeat function for session

--- src/gamesession.rs
+++ src/gamesession.rs
@@ -2,11 +2,12 @@
 * Implements the player actors; these are handled by the server
 *
 *
-* Implements StreamHandler to take in information
+* Implements StreamHandler to take in information from the player.
 */
 
 use std::time::{Duration, Instant};
 use actix::*;
+use actix_files as fs;
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
 
@@ -21,7 +22,7 @@ struct GameSession {
     // holds a unique player uuid
     id: usize,
 
-    // holds heartbeat to check if player is still connected
+    // holds heartbeat to check if player is still connected (Instant is a point in time)
     hb: Instant,
 
     // holds the game room that is joined
@@ -32,5 +33,64 @@ struct GameSession {
     
     // has reference to current card in hand ? Or can let the server take care of this. Needs a referece to the server however
     addr: Addr<server::GameServer>,
+}
+
+impl Actor for GameSession {
+    type Context = ws::WebsocketContext<Self>;
+
+    // Method is called when gamesession spun up; register with the gameserver
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.hb(ctx); // this is the method, not the field; confusing?
+
+        // register with the gameserver (gotten via the addr field)
+        // there's two addr variables here, which is confusing
+        let addr = ctx.address();
+        self.addr
+            .send(server::Connect {
+                addr: addr.recipient(),
+            })
+            .into_actor(self)
+            .then(|res, act, ctx| {
+                match res {
+                    Ok(res) => act.id = res,
+                    _ => ctx.stop(),
+                }
+                fut::ready(())
+            })
+            .wait(ctx);
+
+         
+
+    }
+}
+
+impl Handler<server::Message> for GameSession {
+    type Result = ();
+
+    fn handle(&mut self, msg: server::Message, ctx: &mut Self::Context) {
+        ctx.text(msg.0);
+    }
+
+}
+
+impl GameSession {
+    fn hb(&self, ctx: &mut ws::WebsocketContext<Self>) {
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            
+            if Instant::now().duration_since(act.hb) > Duration::new(10, 0) {
+                // heartbeat timed out
+                println!("Websocket Client heartbeat failed, disconnecting!");
+
+                // How do we get a disconnected actor to rejoin the same game?
+                act.addr.do_send(server::Disconnect {id: act.id});
+
+                ctx.stop();
+
+                return;
+            }
+
+            ctx.ping(b"");
+        });
+    }
 }
 
--- src/server.rs
+++ src/server.rs
@@ -2,7 +2,7 @@
 * Implements the server as an actor. This manages the gamesessions and their playing states.
 * 
 * Might want to create another actor that serves as 'dealer' for a single game.
-*
+* Allow people to join the room until someone starts the game.
 */
 use deckofcards::{Cards, Deck};
 
@@ -17,12 +17,97 @@ use std::collections::{HashMap, HashSet};
 #[rtype(result="()")]
 pub struct Message(pub String);
 
+#[derive(Message)]
+#[rtype(usize)]
+pub struct Connect {
+    pub addr: Recipient<Message>,
+}
+
+/// Session is disconnected
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Disconnect {
+    pub id: usize,
+}
 
 pub struct GameServer {
     sessions: HashMap<usize, Recipient<Message>>,
-    rooms: HashMap<usize, HashSet<usize>>,
+    rooms: HashMap<String, HashSet<usize>>,
     rng: ThreadRng,
 }
+
+// to_owned converts to a string?
+impl GameServer {
+    /// send message to all users in the room (this is horrendous, need to create
+    /// some type of room struct, and just send the message out that way? )
+    fn send_message(&self, room: &str, message: &str, skip_id: usize) {
+        if let Some(sessions) = self.rooms.get(room) {
+            for id in sessions {
+                if *id != skip_id {
+                    if let Some(addr) = self.sessions.get(id) {
+                        let _ =addr.do_send(Message(message.to_owned()));
+                    }
+                }
+            }
+        }
+    }
+}
+
+// implement default method (instantiates the object)
+impl Handler<Connect> for GameServer {
+    type Result = usize;
+
+    fn handle(&mut self, msg: Connect, _: &mut Context<Self>) -> Self::Result {
+        println!("Someone joined");
+
+        // notify all users in same room
+        self.send_message(&"Main".to_owned(), "Someone joined", 0);
+
+        // register session with random id
+        let id = self.rng.gen::<usize>();
+        self.sessions.insert(id, msg.addr);
+
+        // auto join session to Main room
+        self.rooms
+            .entry("Main".to_owned())
+            .or_insert(HashSet::new())
+            .insert(id);
+
+        // send id back
+        id
+    }
+}
+
+impl Handler<Disconnect> for GameServer {
+    type Result = ();
+
+    // remove the uid from the map of sessions -- have to iterate through the rooms?
+    // that's really fucking bad
+    fn handle(&mut self, msg: Disconnect, _: &mut Context<Self>) {
+        println!("Someone disconnected");
+
+        let mut room = String::new();
+
+        //remove address; currently assume player is in only one game.
+        if self.sessions.remove(&msg.id).is_some() {
+            // remove session from all rooms; can't we just query if name is in hashmap?
+            for (name, sessions) in &mut self.rooms {
+                if sessions.remove(&msg.id){
+                    room = name.to_owned();
+                    break;
+                }
+            }
+        }
+
+        /// can get their user id / name here -- need to make Disconnect message 
+        /// store the user name
+        self.send_message(&room, "Someone disconnected", 0);
+
+
+
+    }
+}
+
 
 impl Actor for GameServer {
     /// Just need simple capability to communicate with the other actors.
