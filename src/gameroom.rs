57fd98a44feebaa88d2c4ccc58013abede53e4c2
tsudijon <timothysudijono@gmail.com>
2020-04-26 18:49:36 -0400

    Revert "initialize cards in game"
    This reverts commit c8e90a5924cb5b1b4d58931fd39446781ef8ff30.

--- src/gameroom.rs
+++ src/gameroom.rs
@@ -3,21 +3,20 @@
 /// 
 /// A game room is an actor that contains references to each of the sessions / players
 /// that it contains.
-use deckofcards::{Card, Cards, Deck};
+use deckofcards::{Cards, Deck};
 
 use actix::prelude::*;
 use rand::{self, rngs::ThreadRng, Rng};
-use std::collections::{HashMap};
+use std::collections::{HashMap, HashSet};
 
 use crate::server::Message;
 
 pub struct GameRoom {
     pub id: usize,
     pub sessions: HashMap<usize, Recipient<Message>>,
-    pub playerCards: HashMap<usize, Card>,
+    pub deck: Deck,
     pub firstRoundGuess: HashMap<usize, usize>,
     pub secondRoundGuess: HashMap<usize, String>,
-    //pub sessionOrder: Vec<usize>,
 }
 
 // need to handle someone joining the room; update their information - this is done in the server mod
@@ -26,7 +25,7 @@ impl Default for GameRoom {
         GameRoom {
             id: 0,
             sessions: HashMap::new(),
-            playerCards: HashMap::new(),
+            deck: Deck::new(),
             firstRoundGuess: HashMap::new(),
             secondRoundGuess: HashMap::new(),
         }
@@ -39,51 +38,24 @@ impl Actor for GameRoom {
 
 impl GameRoom {
 
-    // function to get data from a player; needs to be async?
-    async fn poll_session(&mut self, sessionId: usize) {
-
-    }
-
-    // tensor programming had a function in which you can use the await function in a non async function? In webcrawler video.
-
-    fn start_game(&mut self) {
+    async fn startGame(&mut self) {
         // decide on the order for the players
+        self.firstRound()
+            .await;
 
-        // deal the cards, update the player cards
-        let mut deck = Deck::new();
-
-        // need to import the cards trait to make this work.
-        deck.shuffle();
-        for id in self.sessions.keys() {
-            self.playerCards.insert(
-                *id, //* dereferences?
-                deck.deal_one().unwrap(),
-            );
-        }
-
-        // reveal duplicates
-        // push method to all the actors.
-
-        // send messages to the sessions about the other cards.
-        // I think just a do_send to the recipient will do.
-
-        /// match on output of firstRound
-        /// We need to 
-        self.first_round();
-
-        self.second_round();
+        self.secondRound()
+            .await;
 
 
     }
 
     // should return OK if everything went through, otherwise should return fail, or game restart.
-    // I don't think these need to be async functions since they run serially no concurrently.
-    fn first_round(&mut self) {
+    async fn firstRound(&mut self) {
 
     }
 
     // should return OK if everything went through, otherwise should return fail, or game restart.
-    fn second_round(&mut self) {
+    async fn secondRound(&mut self) {
 
     }
 
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,41 @@ mod gameroom;
 fn main() {
     println!("Welcome to HiLo!");
 
+    //prep the game
+    let mut deck = Deck::new();
+    deck.shuffle();
+
+    const NUM_PLAYERS: usize = 8;
+
+    let dealt_cards = deck.deal(NUM_PLAYERS);
+
+    // calculate rank order of the cards
+
+    // need some vector to hold player guesses
+
+
+    // Instantiate players
+
+
+
+    //first round
+    let mut first_round_guesses: [i32; NUM_PLAYERS] = [0; NUM_PLAYERS];
+
+
+    //second round
+    // do we want type to be string (for rank) 
+    let mut second_round_guesses: [char; NUM_PLAYERS] = ['2'; NUM_PLAYERS];
+    let mut group_score = 0;
+
+    //print out
+    for _ in 0..NUM_PLAYERS {
+
+    	// take in player guesses
+    	group_score += 1;
+
+    };
 
     //output score
+
+    deck.reset()
 }
