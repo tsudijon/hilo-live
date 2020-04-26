be63d39c2f3e2cfdcb3b6429ea438ddc0fb03d8a
tsudijon <timothysudijono@gmail.com>
2020-04-26 18:49:43 -0400

    Revert "Added deck class"
    This reverts commit dd7b580dbc9213d03744fb5569e9e462dd5b5147.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-use std::io;
-use deckofcards::*;
-
 fn main() {
-    println!("Welcome to HiLo!");
-
-    //prep the game
-    let mut deck = Deck::new();
-    deck.shuffle();
-
-
-    //first round
-
-    //second round
-    let mut group_score = 0;
-
-    //output score
+    println!("Hello, world!");
 }
